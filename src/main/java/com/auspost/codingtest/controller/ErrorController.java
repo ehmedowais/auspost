b32d9b941cba138640dc8b67c3de0277d74e732e
ehmedowais <ehmedowais@yahoo.com>
2021-08-11 18:15:20 +1000

    Adding CorrelationId

---
+++ src/main/java/com/auspost/codingtest/controller/ErrorController.java
@@ -0,0 +1,37 @@
+package com.auspost.codingtest.controller;
+
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+public class ErrorController extends AbstractErrorController {
+    public ErrorController(ErrorAttributes errorAttributes) {
+        super(errorAttributes);
+    }
+
+    @GetMapping("/error")
+    public ResponseEntity<String> handleErrors()
+    {
+        String responseHtml = "<html><h1>There is an error in ur request</h1></html>";
+        return new ResponseEntity<String>(responseHtml, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @RequestMapping
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        Map<String, Object> body = getErrorAttributes(request,true);
+        HttpStatus status = getStatus(request);
+        return new ResponseEntity<>(body, status);
+    }
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+}
